f6adc0593708a35926a24ed43f16826ad6bbf08c
Zohaib Munir <zohaibmunir32@gmail.com>
2023-01-14 02:11:44 +0500

    Fixed Course Page Issues

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -29,7 +29,7 @@ function Course(props){
         }, [])
         useEffect(()=>{async function getProgresses(){
             let progresses=[];
-            if(isLogin){
+            if(User){
             for(let i=0;i<User.progresses.length;i++){
                    await axios.get("https://educate-pakistan-server.herokuapp.com/progresses/"+User.progresses[i])
                     .then(res=>{
@@ -61,7 +61,7 @@ if(isLogin){
                 <div class="lesson">
                 <a class="lesson-name"href={"/lessons/"+lessons[i].id}>{lessons[i].title}</a>
                   <p class="lesson-description">{lessons[i].description}</p>
-                {isLogin?<progress class="lesson-progress" value={progress.points} max={lessons[i].points}></progress>:<p>Login to see Progress</p>}
+                {isLogin && progress?<progress class="lesson-progress" value={progress.points} max={lessons[i].points}></progress>:<p>Login to see Progress</p>}
                 </div> 
               </div>)
         }
